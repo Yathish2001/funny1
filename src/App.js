208538ad9ba13a2cce0e4a8933c91538ad7b3631
Yathish Kumar B <yathishgowdab2001@gmail.com>
2026-04-27 11:51:37 +0530

    Move App.js to src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// Your updated code for App.js here
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
